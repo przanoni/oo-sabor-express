d867b84f0e1314cebc0366b412af849b864a9ce4
Paulo Zanoni <paulo.zanoni@gmail.com>
2025-10-28 19:25:13 -0300

    Atualizado dia 28/10

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -14,21 +14,31 @@ class Restaurante:
     
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliações'.ljust(25)} | {'Status'}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}')
         
     def alternar_estado(self):
         self._ativo = not self._ativo
 
     def receber_avaliacao(self, cliente, nota):
-        avaliacao  = Avaliacao(cliente, nota)
-        self._avaliacao.append(avaliacao)
+        if 0 < nota <= 5:
+            avaliacao  = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
 
 
     @property
     def ativo(self):
         return '☑' if self._ativo else '☐'
+    
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return '-'
+        soma_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_nota = len(self._avaliacao)
+        media = round(soma_notas / quantidade_nota, 1)
+        return media
     
 
         
